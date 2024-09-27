9eadb7cb14632f522ea78e9f8c6adc3806d542f8
Legion <seiftarek2424@gmail.com>
2024-09-27 21:32:08 +0300

    Created Tourist Schema with validation and the post API

---
+++ server/Model/Queries/tourist_queries.ts
@@ -0,0 +1,8 @@
+import { hashPassword } from "../../utils/functions/bcrypt_functions";
+import touristModel from "../Schemas/Tourist";
+import {isUniqueUsernameAndEmail} from './guest_queries';
+
+
+
+module.exports = { }; 
+
