bb08f5bc398241e6176811bbe50a64fdf152dc05
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-09-27 19:20:06 +0300

    Add TourGuide Interface

--- server/Model/Schemas/TourGuide.ts
+++ server/Model/Schemas/TourGuide.ts
@@ -1,39 +1,25 @@
 import mongoose from "mongoose";
+import { ITourGuide } from "../../Interfaces/Users/ITourGuide";
 
 const Schema = mongoose.Schema;
 
-export interface ITourGuide extends Document {
-  username: string;
-  email: string;
-  password: string;
-  mobileNumber?: string;
-  yearsOfExperience?: number;
-  previousWork: {
-    companyName: string;
-    startDate: Date;
-    endDate: Date;
-    role: string;
-    location: string;
-    description: string;
-  }[];
-  isAccepted: boolean;
-}
 const tourGuideSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  mobileNumber:  String,
-  yearsOfExperience:  Number,
-  previousWork: [{
-    companyName: String,
-    startDate: Date,
-    endDate: Date,
-    role: String,
-    location: String,
-    description: String
-}],
-  isAccepted: { type: Boolean, default: false }
+  mobileNumber: String,
+  yearsOfExperience: Number,
+  previousWork: [
+    {
+      companyName: String,
+      startDate: Date,
+      endDate: Date,
+      role: String,
+      location: String,
+      description: String,
+    },
+  ],
+  isAccepted: { type: Boolean, default: false },
 });
 
-export default mongoose.model<ITourGuide>('TourGuide', tourGuideSchema);
-
+export default mongoose.model<ITourGuide>("TourGuide", tourGuideSchema);
--- server/Routes/TourGuide.ts
+++ server/Routes/TourGuide.ts
@@ -1,45 +1,44 @@
-import {createUser, handleRegisterErrors} from '../Model/Queries/guest_queries';
-import { Request, Response,Router } from 'express';
-import { guestAddValidator } from '../utils/express-validator/GuestValidator';
-import { matchedData,validationResult} from 'express-validator';
-import { getprofileInfo } from '../Model/Queries/user_queries';
-import tourguidemodel, { ITourGuide } from '../Model/Schemas/TourGuide';
+import {
+  createUser,
+  handleRegisterErrors,
+} from "../Model/Queries/guest_queries";
+import { Request, Response, Router } from "express";
+import { guestAddValidator } from "../utils/express-validator/GuestValidator";
+import { matchedData, validationResult } from "express-validator";
+import { getprofileInfo } from "../Model/Queries/user_queries";
+import tourguidemodel from "../Model/Schemas/TourGuide";
+import { ITourGuide } from "../Interfaces/Users/ITourGuide";
 const router = Router();
 
-router.post('/add',guestAddValidator, async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    const newUser = matchedData(req);
-    try {
-        await createUser(newUser,'tourGuide');
-        res.status(201).send({ message: 'User created successfully' });
-    } catch (error) {
-        const err = error as any;
-        handleRegisterErrors(err, res);
-    }
+router.post("/add", guestAddValidator, async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const newUser = matchedData(req);
+  try {
+    await createUser(newUser, "tourGuide");
+    res.status(201).send({ message: "User created successfully" });
+  } catch (error) {
+    const err = error as any;
+    handleRegisterErrors(err, res);
+  }
 });
 
-router.get("/profile/:username",async (req: Request, res: Response)=>{
-try{
+router.get("/:username", async (req: Request, res: Response) => {
+  try {
     const username = req.params.username;
-    const user = await getprofileInfo(username, "tourGuide") as ITourGuide ;
-    if(!user)
-         res.status(404).send("user not found");
+    const user = (await getprofileInfo(username, "tourGuide")) as ITourGuide;
+    if (!user) res.status(404).send("user not found");
     else {
-        console.log(user.isAccepted);
-        res.status(200).send(user);
-    
-
+      if (user.isAccepted) {
+        return res.status(200).json(user);
+      }
+      res.status(403).json(user);
     }
-}
-catch(err){
-   res.status(500).send("error getting user profile");
-}
-}
-);
-     
-
+  } catch (err) {
+    res.status(500).send("error getting user profile");
+  }
+});
 
 export default router;
