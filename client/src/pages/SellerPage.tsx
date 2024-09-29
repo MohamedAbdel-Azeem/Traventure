65b3b06b0d89c7038b8f7277a1a542a3ee1ebf74
Naefu <ahmoslamy@hotmail.com>
2024-09-29 22:35:04 +0300

    added all pages

---
+++ client/src/pages/SellerPage.tsx
@@ -0,0 +1,27 @@
+import Navbar from "../components/navbar";
+import { useState } from "react";
+import Dashboard from "../components/Admin/Dashboard";
+import ImprovedSidebar from "../components/Admin/ImprovedSidebar";
+const SellerPage = () => {
+   const [content, setContent] = useState(<Dashboard/>);
+
+
+    return (
+        <div>
+            <Navbar
+                sideBarFlag={true}
+            />
+            <ImprovedSidebar/>
+            <div
+                style={{
+                    margin: `20px 20px 20px 100px`,
+                    transition: "200ms",
+                }}
+            >
+                {content}
+            </div>
+        </div>
+    );
+};
+
+export default AdminPage;
