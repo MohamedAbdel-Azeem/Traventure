3297ff53ad3da853a4889e57a32eba90dc2e2a02
OHarridy <oelharridy@gmail.com>
2024-09-30 23:09:17 +0300

    CRUD tested for tour guide

--- server/Model/Schemas/Itinerary.ts
+++ server/Model/Schemas/Itinerary.ts
@@ -10,24 +10,32 @@ import mongoose from "mongoose";
 const schema = mongoose.Schema;
 
 const itinerarychema = new schema({
-    main_Picture: {type: String},
+    main_Picture: { type: String },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    added_By:{type: String, required: true },
-    pictures: { type: [String]},
-    price:{type: Number, required:true},
-    starting_Date: {type: Date, required:true},
-    ending_Date: {type: Date, required:true},
-    rating: {type: Float64Array, required:true},
-    total: {type: Number, required:true},
-    language:{type: String, required:true},
-    places:[{
-        place_id: { type: String, required: true ,ref: 'Place'},
+    added_By: { type: String, required: true },
+    price: { type: Number, required: true },
+    starting_Date: { type: Date, required: true },
+    ending_Date: { type: Date, required: true },
+    rating: { type: Number, required: true },
+    total: { type: Number, required: true },
+    language: { type: String, required: true },
+    pickup_location: { type: String, required: true },
+    dropoff_location: { type: String, required: true },
+    places: [{
+        place_id: { type: String, ref: 'Place' },
+        //add order to plan el order of timeline
     }],
-    activities:[{
-        activity_id: { type: String, required: true , },
-    }]
-    //ToDO add ref for activity once it is done
-  });
-  
-  export default mongoose.model("Itinerary", itinerarychema);
+    activities: [{
+        activity_id: { type: String, ref: 'Activity' },
+        //activity_duration: {} to be asked  
+        //add order to plan el order of timeline
+        // ne add date for each activity w ne sort it by date and somehow make both in one array
+    }],
+    booked_By: [{
+        user_id: { type: String, ref: 'Tourist' }
+    }],
+    accesibility: { type: Boolean, required: true }
+});
+// ToDo: plan a way to do timeline
+export default mongoose.model("Itinerary", itinerarychema);
