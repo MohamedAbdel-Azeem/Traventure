5ca479758aa02262d4e6b2bb79436c8b832e4baf
Naefu <ahmoslamy@hotmail.com>
2024-09-30 12:37:29 +0300

    quick delete

--- client/src/components/TourismGovernor/TGoDashboard.tsx
+++ client/src/components/TourismGovernor/TGoDashboard.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import LocationTable from '../LocationTable';
 const TGoDashboard = () => {
 
 
 
     return (
         <div className="grid grid-cols-3">
-            <LocationTable/>
         </div>
     );
 };
