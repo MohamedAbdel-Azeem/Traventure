d06b015bcbd8f35c98fa4949111cccac774dc576
LUKITA1416171822 <132844508+LUKITA1416171822@users.noreply.github.com>
2024-10-01 22:03:16 +0300

    minor chagnes in schemas

--- server/utils/express-validator/touristValidator.ts
+++ server/utils/express-validator/touristValidator.ts
@@ -15,7 +15,7 @@ export const touristAddValidator = [
     .withMessage("Password must be at least 8 characters long"),
     body("mobileNumber")
     .isString()
-    .isLength({ min: 11, max:11 })
+    .isLength({ min: 12, max:14 })
     .withMessage("Mobile number must be exactly 11 characters long"),
     body("dateOfBirth")
     .isString()
@@ -31,7 +31,7 @@ export const touristAddValidator = [
 export const touristUpdateValidator = [
     body('email').isEmail().withMessage('Invalid email').optional(),
     body('password').isString().isLength({ min: 8 }).withMessage('Password must be at least 8 characters long').optional(),
-    body('mobileNumber').isString().isLength({ min: 11, max: 11 }).
+    body('mobileNumber').isString().isLength({ min: 12, max: 14 }).
     withMessage('Mobile number must be exactly 11 characters long').optional(),
     body('Occupation').isString().withMessage('Invalid Occupation').optional(),
     body('nationality').isString().withMessage('Invalid Nationality').optional()
