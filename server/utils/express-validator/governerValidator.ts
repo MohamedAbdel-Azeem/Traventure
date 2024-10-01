0a0fcaf25223bf239e4e17c5625d2c7f1c1b32db
LUKITA1416171822 <132844508+LUKITA1416171822@users.noreply.github.com>
2024-10-01 22:33:02 +0300

    updated Advertiser schema

--- server/utils/express-validator/governerValidator.ts
+++ server/utils/express-validator/governerValidator.ts
@@ -3,14 +3,10 @@ import { body } from "express-validator";
 
 export const governerValidator = [
   body("username").isString().withMessage("Username must be a string")
-  .isLength({ min: 3 , max: 20 }).withMessage("Username must be between 3 and 20 characters long"),
+  .isLength({ min: 3}).withMessage("Username must be between 3 and 20 characters long"),
 
     body("password").isString().withMessage("Password must be a string")
-    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
-    .matches(/[A-Z]/).withMessage("Password must contain at least one uppercase letter")
-    .matches(/[a-z]/).withMessage("Password must contain at least one lowercase letter")
-    .matches(/\d/).withMessage("Password must contain at least one number")
-    .matches(/[\W_]/).withMessage("Password must contain at least one special character"),
+  .isLength({ min: 8 }).withMessage("Password must be at least 8 characters long"),
 
 ];
 
