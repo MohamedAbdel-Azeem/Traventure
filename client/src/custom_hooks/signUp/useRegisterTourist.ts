4583bc2136306f5e705b0e9aefcfdd13e2facd16
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-10-03 21:30:58 +0300

    add custom hook for tourist signup

---
+++ client/src/custom_hooks/signUp/useRegisterTourist.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useRegisterTourist(body: object | null, role: string) {
+  const url = "traventure/api/tourist/add";
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (role !== "tourist") return;
+      if (body === null) return;
+      console.log(body);
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await axios.post(url, body);
+        if (response.status >= 200 && response.status < 300) {
+          setData(response.data);
+          console.log(response.data);
+        } else {
+          throw new Error("Server can't be reached!");
+        }
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [body, role]);
+  return { data, loading, error };
+}
+
+export default useRegisterTourist;
