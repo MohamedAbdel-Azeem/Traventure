940ccfa8f2ed69865f353ca993e01e6b2b048309
OHarridy <oelharridy@gmail.com>
2024-11-02 22:17:15 +0200

    Add pdf uploader link in backend for schemas seller, advertiser, and tourguide

--- server/Model/Schemas/Seller.ts
+++ server/Model/Schemas/Seller.ts
@@ -6,6 +6,7 @@ const SellerSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  documents: { type: String, required: true },
   name: String,
   description: String,
   isAccepted: { type: Boolean, default: false },
