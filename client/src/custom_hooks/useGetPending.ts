2e65d25c2f5ef578d3ef67f7274e9625445fbbb0
OHarridy <oelharridy@gmail.com>
2024-11-04 17:52:04 +0200

    adjust the custom hook for getting all pending users

--- client/src/custom_hooks/useGetPending.ts
+++ client/src/custom_hooks/useGetPending.ts
@@ -1,11 +1,40 @@
 import axios from "axios";
+import { ITourGuide } from "../routes/_app/tourguide_profile/ITourGuide";
+import { ISeller } from "../routes/_app/seller_profile/ISeller";
+import { IAdvertiser } from "../routes/_app/advertiser_profile/IAdvertiser";
+import { useEffect, useState } from "react";
 
-export  const getAllPendingUsers = async () => {
-    const url = `/traventure/api/admin/pendingusers`;
-    try {
-      const { data } = await axios.get(url);
-      return data;
-    } catch (err) {
-     return "error fetching pending users" ;
-    } 
-  };
+type Pending = {
+    tourGuides: ITourGuide[],
+    sellers: ISeller[],
+    advertisers: IAdvertiser[]
+}
+
+export function getAllPendingUsers() {
+    const [pendingdata, setpendingdata] = useState<Pending[]|null>(null);
+    const [pendingloading, setpendingLoading] = useState(true);
+    const [pendingerror, setpendingError] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            setpendingLoading(true);
+            setpendingError(null);
+            try {
+                const response = await axios.get(`/traventure/api/admin/pendingusers`);
+                if (response.status >= 200 && response.status < 300) {
+                    setpendingdata(response.data);
+                }
+                else {
+                    throw new Error("Server can't be reached!");
+                }
+            }
+            catch (error : any) {
+                setpendingError(error.message);
+            }
+            finally {
+                setpendingLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+    return {pendingdata, pendingloading, pendingerror };
+}
