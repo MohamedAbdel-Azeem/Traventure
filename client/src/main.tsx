684ac49c3cf781dad316894b66d1d149101bbec9
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-11-14 17:49:05 +0200

    add redux-persist and js-cookie for state persistence; refactor store configuration to use persisted reducer with cookie storage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,18 +6,43 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import exchangeRateReducer from "./redux/exchangeRateSlice.ts";
 import cartReducer from "./redux/cartSlice.ts";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { CookieStorage } from "redux-persist-cookie-storage"; // Correct import
+import Cookies from "js-cookie";
+import { combineReducers } from "redux";
 
-const store = configureStore({
-  reducer: {
-    exchangeRate: exchangeRateReducer,
-    cart: cartReducer,
-  },
+// Persist configuration
+const persistConfig = {
+  key: "root",
+  storage: new CookieStorage(Cookies, {
+    expiration: {
+      default: 3600, // 1 hour in seconds
+    },
+  }),
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  exchangeRate: exchangeRateReducer,
+  cart: cartReducer,
 });
 
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
