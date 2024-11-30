c7a84043d4ac228d48bb9c0b0a03055f22009851
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-11-30 17:09:59 +0200

    Update promo code scheduler to run daily at 1 PM

--- server/utils/functions/node_scheduler.ts
+++ server/utils/functions/node_scheduler.ts
@@ -3,7 +3,7 @@ const schedule = require("node-schedule");
 
 // Schedule a function to run every day at 3 PM
 module.exports = () => {
-  schedule.scheduleJob("08 17 * * *", async () => {
+  schedule.scheduleJob("00 13 * * *", async () => {
     console.log("Running promo code scheduler");
     handlePromoCode();
   });
