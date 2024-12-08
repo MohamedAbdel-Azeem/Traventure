d78123a878fe4f885f0322f46de90156b162c4bd
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-12-08 20:35:07 +0200

    Add function to send email with PDF attachment

--- server/utils/functions/email_sender.ts
+++ server/utils/functions/email_sender.ts
@@ -1,4 +1,5 @@
 var nodemailer = require("nodemailer");
+const axios = require("axios");
 require("dotenv").config();
 
 interface ImailOptions {
@@ -26,6 +27,41 @@ export function sendMail(email: string, subject: string, textHtml: string) {
     subject: subject,
     html: textHtml,
   };
+  transporter.sendMail(mailOptions, function (error: any, info: any) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
+
+// pdf is provided as a link
+export function sendMailWithPdf(
+  email: string,
+  subject: string,
+  textHtml: string,
+  pdfLink: string
+) {
+  const response = await axios({
+    method: "get",
+    url: pdfLink,
+    responseType: "arraybuffer",
+  });
+
+  const mailOptions = {
+    from: process.env.GMAIL_ADDRESS,
+    to: email,
+    subject: subject,
+    html: textHtml,
+    attachments: [
+      {
+        filename: "invoice.pdf",
+        content: response.data,
+      },
+    ],
+  };
+
   transporter.sendMail(mailOptions, function (error: any, info: any) {
     if (error) {
       console.log(error);
