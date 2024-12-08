e13a98a4a243cf2371e942aa83b13233975911c3
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-12-08 17:52:55 +0200

    Remove commented-out loyalty points update function from purchase queries

--- server/Model/Queries/purchase_queries.ts
+++ server/Model/Queries/purchase_queries.ts
@@ -59,34 +59,6 @@ export async function getPurchaseTotalAmount(purchaseData: IPurchase) {
   }
 }
 
-// export async function updateLoyaltyPoints(touristId:string, amount:number){
-//   try {
-//     const tourist = await touristModel.findById(touristId);
-//     if (!tourist) return null;
-//     var points=0;
-//     switch(tourist.loyaltyLevel){
-//       case 1: points=amount*0.5; break;
-//       case 2: points=amount; break;
-//       case 3: points=amount*1.5; break;
-//     }
-//     tourist.currentLoyaltyPoints += points;
-//     tourist.totalLoyaltyPoints += points;
-
-//     if(tourist.totalLoyaltyPoints>500000){
-//       tourist.loyaltyLevel=3;
-//     }
-//     else if(tourist.totalLoyaltyPoints>100000){
-//       tourist.loyaltyLevel=2;
-//     }
-//     else {
-//       tourist.loyaltyLevel=1;
-//     }
-//     await tourist.save();
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
 export async function getTouristPurchases(
   touristId: string | mongoose.Types.ObjectId
 ) {
@@ -406,7 +378,6 @@ module.exports = {
   getTouristPurchases,
   getSellerSales,
   getExternalSellerSales,
-  // updateLoyaltyPoints,
   getPurchaseTotalAmount,
   cancelPurchase,
   DeliverPurchase,
