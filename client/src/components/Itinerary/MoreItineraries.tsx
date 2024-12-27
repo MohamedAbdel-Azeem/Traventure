79ddc0acb48bbdd3f87a7b9f7ed5ab84e6f42cc1
Ziad Raafat <zrafat65@gmail.com>
2024-12-27 02:43:57 +0200

    .

--- client/src/components/Itinerary/MoreItineraries.tsx
+++ client/src/components/Itinerary/MoreItineraries.tsx
@@ -344,7 +344,6 @@ const MoreItineraries: React.FC = () => {
         <br />
         <div>
           
-          <hr />
 
           <div className="overflow-x-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
